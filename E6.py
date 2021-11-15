edd3ba843245660a26a432d38785a6ff70d4689e
shayen17 <31423783+shayen17@users.noreply.github.com>
2021-11-15 17:45:08 -0300

    Add files via upload

--- E6.py
+++ E6.py
@@ -1,113 +1,80 @@
-'''Dada una lista de superhéroes de comics, de los cuales se conoce su nombre, año aparición,
+from grafo import Grafo
 
-casa de comic a la que pertenece (Marvel o DC) y biografía, implementar la funciones necesa-
-rias para poder realizar las siguientes actividades:
+dioses = Grafo(dirigido=False)
 
-a. eliminar el nodo que contiene la información de Linterna Verde;
-b. mostrar el año de aparición de Wolverine;
-c. cambiar la casa de Dr. Strange a Marvel;
-d. mostrar el nombre de aquellos superhéroes que en su biografía menciona la palabra
-“traje” o “armadura”;
-e. mostrar el nombre y la casa de los superhéroes cuya fecha de aparición
-sea anterior a 1963;
-f. mostrar la casa a la que pertenece Capitana Marvel y Mujer Maravilla;
-g. mostrar toda la información de Flash y Star-Lord;
-h. listar los superhéroes que comienzan con la letra B, M y S;
-i. determinar cuántos superhéroes hay de cada casa de comic.'''
+dioses.insertar_vertice('Urano')
+dioses.insertar_vertice('Cronos')
+dioses.insertar_vertice('Rea')
+dioses.insertar_vertice('Zeus')
+dioses.insertar_vertice('Hades')
+dioses.insertar_vertice('Demeter')
+dioses.insertar_vertice('Atenea')
+dioses.insertar_vertice('Apollo')
+dioses.insertar_vertice('Persefone')
 
-from lista import Lista
+dioses.insertar_arista(1, 'Urano', 'Cronos', data={'relacion': ['padre', 'hijo']})
 
-lista_personajes = Lista()
-personajes = [
-    {'name':'Iron man','Anio': 1958, 'Casa' : 'Marvel', 'Biografia' : 'Traje'},
-    {'name':'Wolverine','Anio': 1950, 'Casa' : 'Marvel', 'Biografia' : 'Traje'},
-    {'name':'Hulk','Anio': 2002, 'Casa' : 'Marvel', 'Biografia' : 'Es verde'},
-    {'name':'Linterna verde','Anio': 2003, 'Casa' : 'DC', 'Biografia' : 'Armadura'},
-    {'name':'Dr Strange','Anio': 2004, 'Casa' : 'DC', 'Biografia' : 'Armadura'},
-    {'name':'Star Lord','Anio': 2005, 'Casa' : 'DC', 'Biografia' : 'Armadura'},
-    {'name':'Mujer Maravilla','Anio': 2006, 'Casa' : 'DC', 'Biografia' : 'traje'},
-    {'name':'Capitana Marvel','Anio': 555, 'Casa' : 'Marvel', 'Biografia' : 'ropita'},
-    {'name':'Blade','Anio': 2007, 'Casa' : 'Marvel', 'Biografia' : 'Traje'},
-    {'name':'Flash','Anio': 2005, 'Casa' : 'Marvel', 'Biografia' : 'Armadura'},
-]
+dioses.insertar_arista(1, 'Cronos', 'Zeus', data={'relacion': ['padre', 'hijo']})
+dioses.insertar_arista(1, 'Cronos', 'Hades', data={'relacion': ['padre', 'hijo']})
+dioses.insertar_arista(1, 'Cronos', 'Demeter', data={'relacion': ['padre', 'hijo']})
+dioses.insertar_arista(1, 'Cronos', 'Rea', data={'relacion': ['pareja']})
+dioses.insertar_arista(1, 'Cronos', 'Rea', data={'relacion': ['hermano']})
 
+dioses.insertar_arista(1, 'Zeus', 'Hades', data={'relacion': ['hermano']})
+dioses.insertar_arista(1, 'Zeus', 'Atenea', data={'relacion': ['padre', 'hijo']})
+dioses.insertar_arista(1, 'Zeus', 'Apollo', data={'relacion': ['padre', 'hijo']})
+dioses.insertar_arista(1, 'Zeus', 'Persefone', data={'relacion': ['padre', 'hijo']})
 
-for elementos in personajes:
-    lista_personajes.insertar(elementos,'name')
+dioses.insertar_arista(1, 'Demeter', 'Persefone', data={'relacion': ['madre', 'hijo']})
+dioses.insertar_arista(1, 'Apollo', 'Persefone', data={'relacion': ['hermano']})
+
+# origen = dioses.buscar_vertice('Persefone')
+print('amplitud')
+dioses.barrido_amplitud(0)
+print('fin')
 
 
-print('----------------------------')
-#A
-lista_personajes.eliminar('Linterna verde','name')
-print('----------------------------')
-#B
-poswolverine= lista_personajes.busqueda('Wolverine','name')
-print('el anio de wolverine es :',lista_personajes.obtener_elemento(poswolverine)['Anio'])
-print('----------------------------')
-#C
-posDr= lista_personajes.busqueda('Dr Strange','name')
-lista_personajes.obtener_elemento(posDr)['Casa']= 'Marvel'
-print('----------------------------')
-#D
-for i in range(0,lista_personajes.tamanio()):
-    if lista_personajes.obtener_elemento(i)['Biografia'].find('Traje')!= -1 or lista_personajes.obtener_elemento(i)['Biografia'].find('Armadura')!= -1:
-        print(lista_personajes.obtener_elemento(i)['name'])
-print('----------------------------')
-#E
-for i in range(0,lista_personajes.tamanio()):
-    x = lista_personajes.obtener_elemento(i)
-    if x['Anio'] < 1963:
-        print('menores a 1963: ',x['name'],x['Casa'])
-print('----------------------------')
+print(dioses.es_adyacente('Zeus', 'Urano'))
 
-#F
-poscapitana= lista_personajes.busqueda('Capitana Marvel','name')
-print('Casa de capitana marvel :',lista_personajes.obtener_elemento(poscapitana)['Casa'])
-print('----------------------------')
+dioses.adyacentes('Zeus')
 
-posmaravilla= lista_personajes.busqueda('Mujer Maravilla','name')
-print('Casa de Mujer Maravilla :',lista_personajes.obtener_elemento(posmaravilla)['Casa'])
-print('----------------------------')
+def ancestro(vertice_nombre):
+    origen = dioses.buscar_vertice(vertice_nombre)
+    if(origen != -1):
+        dios = dioses.inicio.obtener_elemento(origen)
+        for i in range(dios['aristas'].tamanio()):
+            nombre_dios = dios['aristas'].obtener_elemento(i)['destino']
+            dios_aux = dios['aristas'].obtener_elemento(i)['data']
+            if(len(dios_aux['relacion']) > 1):
+                if(dios_aux['relacion'][1] == 'padre' or dios_aux['relacion'][1] == 'madre'):
+                    print(nombre_dios, dios_aux['relacion'])
+                    ancestro(nombre_dios)
 
-#G
-pos_infoFlash= lista_personajes.busqueda('Flash','name')
-print('Datos de Flash :',lista_personajes.obtener_elemento(pos_infoFlash))
+def directo(ver_origen, ver_destino):
+    print('tiene relacion directa:')
+    pos = dioses.buscar_arista(ver_origen, ver_destino)
+    print(pos)
+    if(pos != 1):
+        pos_aux = dioses.buscar_vertice('Zeus')
+        print(dioses.inicio.obtener_elemento(pos_aux)['aristas'].obtener_elemento(pos))
 
-print('----------------------------')
-pos_infoStar= lista_personajes.busqueda('Star Lord','name')
-print('Datos de Star Lord :',lista_personajes.obtener_elemento(pos_infoStar))
 
-print('----------------------------')
-#punto H
-for i in range(0,lista_personajes.tamanio()):
-    x = lista_personajes.obtener_elemento(i)
-    if (x['name'][0]=="B"):
-        print('Los nombres que empiezan con la letra B son: ',x['name'])
+directo('Zeus', 'Apollo')
+ancestro('Persefone')
 
-for i in range(0,lista_personajes.tamanio()):
-    x = lista_personajes.obtener_elemento(i)
-    if (x['name'][0]=="M"):
-        print('Los nombres que empiezan con la letra M son: ',x['name'])
 
-for i in range(0,lista_personajes.tamanio()):
-    x = lista_personajes.obtener_elemento(i)
-    if (x['name'][0]=="S"):
-        print('Los nombres que empiezan con la letra S son: ',x['name'])
+vertice_origen = dioses.buscar_vertice('Urano')
+vertice_destino = dioses.buscar_vertice('Atenea')
 
-print('----------------------------')
-#Punto I
-contador_casamarvel=0
-contador_casadc=0
-for i in range(0,lista_personajes.tamanio()):
-    x = lista_personajes.obtener_elemento(i)
-    if x['Casa'] == "Marvel":
-        contador_casamarvel=contador_casamarvel+1
-    else:
-        contador_casadc=contador_casadc+1
+camino = dioses.dijkstra(vertice_origen, vertice_destino)
 
-print('La cantidad de superheores de la casa marvel son: ',contador_casamarvel)
-print('La cantidad de superheores de la casa dc son: ',contador_casadc)
-
-print('----------------------------')
-
-lista_personajes.barrido()
+destino = 'Atenea'
+recorrido = None
+while(not camino.pila_vacia()):
+    dato = camino.desapilar()
+    if(dato[1][0] == destino):
+        if(recorrido is None):
+            recorrido = dato[0]
+        print(dato[1][0])
+        destino = dato[1][1]
+print('el costo total es de :', recorrido)
